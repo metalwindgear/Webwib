d10dce18a31dcc68d96299f18eb01ca2c9ae1e9f
metalwindgear <metalgearwind@gmail.com>
2025-09-19 17:12:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Get the button and message elements by their IDs
 const myButton = document.getElementById('myButton');
 const messageElement = document.getElementById('message');
+var hatd = 0
 
 // Add an event listener to the button
 myButton.addEventListener('click', function() {
     // When the button is clicked, change the text of the message element
-    messageElement.textContent = 'Hello, world! You just clicked the button. 😀';
+    hatd += 1
+    if hatd == 1;
+        messageElement.textContent = 'Hello, world! You just clicked the button. 😀';
 });
